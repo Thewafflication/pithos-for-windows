3b9dc12dad2c4fe65f1b9061860ae244a3c450a6
TingPing <tngpng@gmail.com>
2012-06-06 12:35:07 -0400

    finished growl plugin

--- pithos/plugins/growl.py
+++ pithos/plugins/growl.py
@@ -1,6 +1,10 @@
 from pithos.plugin import PithosPlugin
 from pithos.pithosconfig import get_data_file
 import logging
+try:
+	import gntp.notifier
+except(ImportError):
+	logging.error('Growl Error: gntp not installed: https://github.com/kfdm/gntp')
 
 class GrowlPlugin(PithosPlugin):
 	preference = 'growl'
@@ -9,58 +13,33 @@ class GrowlPlugin(PithosPlugin):
 		pass
 
 	def on_enable(self):
-		import gntp.notifier
-
 		#temp hosting of icon
-		pithosicon = 'http://puu.sh/xnMA'
+		self.pithosicon = 'http://puu.sh/xnMA'
 		self.growl = gntp.notifier.GrowlNotifier(
 			applicationName='Pithos',
-			notifications=['State Changed', 'Song Changed'],
+			notifications=['Song Changed'],
 			defaultNotifications=['Song Changed'],
-			applicationIcon=pithosicon,
+			applicationIcon=self.pithosicon,
+			# change for over the network notifications
 			#hostname='localhost',
 			#password=''
 		)
-
-		self.growl.register()
+		try: self.growl.register()
+		except: logging.warning('Failed to register Growl')
 		self.song_callback_handle = self.window.connect("song-changed", self.song_changed)
-		self.state_changed_handle = self.window.connect("user-changed-play-state", self.playstate_changed)
-
-	def playstate_changed(self, window, state):
-		if not self.window.is_active():
-			if state:
-				currstate = 'Playing'
-			else:
-				currstate = 'Paused'
-
-			try:
-				self.growl.notify(
-					noteType='State Changed',
-					title='Pithos ' + currstate,
-					description='',
-					icon=pithosicon,
-					sticky=False,
-					priority=-1
-				)
-			except:
-				logging.warning('Growl not running.')
 
 	def song_changed(self, window, song):
-		if not self.window.is_active():
-			try:
-				self.growl.notify(
-					noteType='Song Changed',
-					title='Now Playing - %s' %(song.title),
-					description='by %s on %s' %(song.artist, song.album),
-					icon=pithosicon,
-					sticky=False,
-					priority=1
-				)
-			except:
-				logging.warning('Growl not running.')
-
+		try:
+			self.growl.notify(
+				noteType='Song Changed',
+				title='Now Playing - %s' %(song.title),
+				description='by %s on %s' %(song.artist, song.album),
+				icon=self.pithosicon,
+				sticky=False,
+				priority=1
+			)
+		except:
+			logging.warning('Growl not running')
 
 	def on_disable(self):
 		self.window.disconnect(self.song_callback_handle)
-		# second disconnect errors?!
-        #self.window.disconnect(self.state_changed_handle)
